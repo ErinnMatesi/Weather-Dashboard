dec9ee0e1e02608b63167d0a45ae4a8bb410d5bb
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-01-31 18:59:33 -0800

    localStorage added for history, working on displaying forecast.

--- assets/script.js
+++ assets/script.js
@@ -2,38 +2,52 @@ var today = moment().format('L');
 var apiKey = "0111ecb58971fc33b9ef27ef46d9d788";
 var searchButton = document.querySelector(".searchButton");
 var searchInput = document.querySelector(".searchInput");
-var searchHistory = document.getElementById("#searchHistory");
+var searchHistory = document.querySelector("#searchHistory");
+var forecast = document.querySelector("#forecast");
+var dayTemp = document.querySelector("#dayTemp");
+var dayWind = document.querySelector("#dayWind");
+var dayHumid = document.querySelector("#dayHumid");
+var dayUV = document.querySelector("#dayUV");
 
-var getWeather;
-var getLongLat;
-var displayResults;
+var displayResults = function(data) {
+    forecast.textContent = city + today;
+    // need to add weather icon to this too ^
+    dayTemp.textContent = "Temp:" + data.current.temp;
+    dayWind.textContent = "Wind:" + data.current.wind_speed;
+    dayHumid.textContent = "Humidity:" + data.current.humidity;
+    dayUV.textContent = "UV:" + data.current.uvi;
+    console.log("it's ALIIIIVVVEEEE")
+};
+
+// variable.textContent = "Temp: " + data.current.temp
 var fiveDay;
-var printHistory;
 
-// pulls value from the input
-var getCity = searchInput.value.trim();
-
-getLongLat = function(city) {
+var getLongLat = function(city) {
     var geoAPI = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=0111ecb58971fc33b9ef27ef46d9d788";
 
     fetch(geoAPI)
     .then(function (response) {
-        // response.json().then(function (data) {
-        //     getWeather(data.lat, data.lon);
-        // })
-        console.log(response.lon);
+        console.log(response);
         return response.json();
     })
     .then(function (data) {
         console.log(data);
+        getWeather(data[0].lat, data[0].lon);
+
+        // check local storage and add new city to list
+        var oldHistory = JSON.parse(localStorage.getItem("history")) || [];
+        if (!oldHistory.includes(data[0].name)) {
+        oldHistory.push(data[0].name)
+        localStorage.setItem("history", JSON.stringify(oldHistory));
+        }
     })
     .catch(function (error) {
-        alert("Unable to find that city");
+        alert("Unable to find that lat/lon");
     });
 };
 
 // fetches weather info in imperial units
-getWeather = function(lat, lon) {
+var getWeather = function(lat, lon) {
     var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=imperial&appid=0111ecb58971fc33b9ef27ef46d9d788";
 
     fetch(apiURL)
@@ -42,24 +56,41 @@ getWeather = function(lat, lon) {
     })
     .then(function (data) {
         console.log(data);
+        // add weather card details here
+        // creating daily forecast
+        displayResults(data);
+        // new Date(data.current.dt*1000).toLocaleDateString()
+        // console.log(Date);
     })
     .catch(function (error) {
         alert("Unable to find that city");
     });
 };
 
-printHistory = function() {
+// new Date(data.current.dt*1000).toLocaleDateString()
+
+
+var printHistory = function(city) {
     var historyItem = document.createElement("li");
-    historyItem.innerText=getCity;
+    historyItem.innerText=city;
+    historyItem.addEventListener("click", function(event) {
+        var city = event.target.innerText;
+        getLongLat(city);
+    });
     searchHistory.appendChild(historyItem);
 };
 
+var getFromStorage = function() {
+    var history = JSON.parse(localStorage.getItem("history")) || [];
+    for (var i = 0; i < history.length; i++) {
+        printHistory(history[i]);
+    }
+};
 
+getFromStorage();
 
 searchButton.addEventListener("click", function() {
+    var getCity = searchInput.value.trim();
     getLongLat(getCity);
-    printHistory();
-    getWeather();
-    displayResults();
-    fiveDay();
+    printHistory(getCity);
 });
